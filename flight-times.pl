8926be2e345d675ec121e7ec93dd4eb61641be02
E. Choroba <choroba@matfyz.cz>
2018-11-25 10:46:19 +0100

    Use real constants

--- flight-times.pl
+++ flight-times.pl
@@ -22,33 +22,33 @@ sub info {
 }
 
 my %events;
-$events{$_}{&W}{&LEAVE}++ for 7, 8.25, 13, 14, 16.5;
-$events{$_}{&B}{&LEAVE}++ for 9.5, 10, 13.5, 15, 16;
+$events{$_}{+W}{+LEAVE}++ for 7, 8.25, 13, 14, 16.5;
+$events{$_}{+B}{+LEAVE}++ for 9.5, 10, 13.5, 15, 16;
 
-my %needed = ( &W => 0, &B => 0 );
+my %needed = ( (W) => 0, (B) => 0 );
 
 sub needed {
-    $needed{&W} + $needed{&B};
+    $needed{+W} + $needed{+B};
 }
 
-my %planes = ( &W => 0, &B => 0 );
+my %planes = ( (W) => 0, (B) => 0 );
 
 while (my @timescale = sort { $a <=> $b } keys %events) {
     my $time = shift @timescale;
     for my $city (W, B) {
 
-        if (my $ready = $events{$time}{$city}{&READY}) {
+        if (my $ready = $events{$time}{$city}{+READY}) {
             info("\t$time $city: $planes{$city} + new $ready ready.");
             $planes{$city} += $ready;
         }
 
-        if (my $leave = $events{$time}{$city}{&LEAVE} // 0) {
+        if (my $leave = $events{$time}{$city}{+LEAVE} // 0) {
             my $missing = $leave - $planes{$city};
             $missing = 0 if $missing < 0;
             $needed{$city} += $missing;
             info("$missing more planes needed") if $missing;
             $events{$time + FLIGHT + TURNAROUND}{ W eq $city ? B : W }
-                {&READY} += $leave;
+                {+READY} += $leave;
             info("\t$time $city: $planes{$city} - $leave leaving");
             $planes{$city} -= $leave;
             $planes{$city} = 0 if $planes{$city} < 0;
